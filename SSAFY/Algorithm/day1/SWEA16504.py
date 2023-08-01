b5992932dda3a0b42026ea6aaa4b2db2c693cee2
Bada35 <diduedidue@naver.com>
2023-08-02 08:15:26 +0900

    230802 8:15

--- SSAFY/Algorithm/day1/SWEA16504.py
+++ SSAFY/Algorithm/day1/SWEA16504.py
@@ -1,8 +1,16 @@
-# def heights(heights):
-#     for height in
-#
-# T = int(input())
-#
-# for tc in range:
-#     N = int(input())
-#     heights = list(map(int, input().split()))
+T = int(input())
+
+for tc in range(1, T + 1):
+    max_height = 0
+    N = int(input())
+    heights = list(map(int, input().split()))
+    for i in range(N):  # heights 요소 인덱싱
+        for j in range(heights[i]):  # 한 height의 1층 2층 3층
+            cnt = 0
+            for k in range(i + 1, N):  # 그 height보다 낮은 층들에 대해
+                if j < heights[k]:
+                    cnt += 1
+            result = N - 1 - i - cnt
+            if result > max_height:
+                max_height = result
+    print(f'#{tc} {max_height}')
