e668f81b1fe79505cd44fa8511305e395fa13065
Jisu Seo <139415935+Bada35@users.noreply.github.com>
2023-08-22 17:36:58 +0900

    [Bronze I] Title: 색종이, Time: 348 ms, Memory: 39044 KB -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -1,13 +1,24 @@
-N = int(input())  # N:색종이 장수
-area = [[0]*1001 for _ in range(1001)]
-res = [0] * (N + 1)
+import sys
+input = sys.stdin.readline
+
+N = int(input())  # 색종이의 개수
+paper_area = [[0 for _ in range(1001)] for _ in range(1001)]
+res = [0] * (N + 1)  # 각 색종이가 보이는 넓이 저장
 
 for n in range(1, N + 1):
-    st_x, st_y, c, r = map(int, input().split())
-    for x in range(c):
-        for y in range(r):
-            res[area[st_x + x][st_y + y]] -= 1
-            area[st_x + x][st_y + y] = n
-            res[n] += 1
+    x, y, width, height = map(int, input().split())
 
-print(*res[1:], sep='\n')
+    # 해당 영역에 색종이 번호로 값을 업데이트
+    for i in range(x, x + width):
+        for j in range(y, y + height):
+            paper_area[i][j] = n
+
+# 각 색종이의 번호가 몇 번 등장하는지 카운트하여 넓이 계산
+for i in range(1001):
+    for j in range(1001):
+        if paper_area[i][j] != 0:
+            res[paper_area[i][j]] += 1
+
+# 출력
+for i in range(1, N + 1):
+    print(res[i])
