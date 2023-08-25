79eb9ea7630fb9a7e47305adea7e0bd679dd12ed
Jisu Seo <139415935+Bada35@users.noreply.github.com>
2023-08-25 15:55:02 +0900

    [Silver IV] Title: 빙고, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2578. 빙고/빙고.py
+++ 백준/Silver/2578. 빙고/빙고.py
@@ -1,7 +1,7 @@
 def fill_bingo(n):
     num_id = Bingo.index(n)
     Bingo.pop(num_id)
-    Bingo.insert(num_id, 0)  # 부른건 0 표시
+    Bingo.insert(num_id, 0)
 
 
 def bingo_check():
@@ -9,23 +9,18 @@ def bingo_check():
     dae1 = []
     dae2 = []
 
-    #가로 체크
     for i in range(5):
         if Bingo[i::5] == [0, 0, 0, 0, 0]:
-            # print('세로카운트+1')
             cnt += 1
         if Bingo[5 * i:5 * (i + 1)] == [0, 0, 0, 0, 0]:
-            # print('가로카운트+1')
             cnt += 1
         dae1.append(Bingo[6 * i])
         dae2.append(Bingo[4 * (i + 1)])
 
     if dae1 == [0, 0, 0, 0, 0]:
         cnt += 1
-        # print('대1카운트+1')
     if dae2 == [0, 0, 0, 0, 0]:
         cnt += 1
-        # print('대2카운트+1')
 
     return cnt
 
@@ -42,17 +37,11 @@ for i in range(5):
 for i in range(5):
     Call += input().split()
 
-# print(Bingo)
-# print(Call)
+for num in Call[:11]:
+    fill_bingo(num)
 
-# for num in Call[:12]:  # 12번까진 일단 부르기(그전엔 빙고 안됨)
-#     fill_bingo(num)
-
-for i in range(25):
+for i in range(11, 25):
     fill_bingo(Call[i])
-    # for i in range(5):
-    #     print(Bingo[5*i:5*(i+1)])
-    # print(bingo_check())
     if bingo_check() >= 3:
         print(i + 1)
         break
