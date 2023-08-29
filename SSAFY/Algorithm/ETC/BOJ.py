4e60024685c0f5d8d5603063fa1b45b0d0a82a1c
Bada35 <diduedidue@naver.com>
2023-08-30 08:22:02 +0900

    11

--- SSAFY/Algorithm/ETC/BOJ.py
+++ SSAFY/Algorithm/ETC/BOJ.py
@@ -1,31 +1,13 @@
-def on_off(s):  # 스위치 번호(인덱스)로 받기
-    if switch[s]:
-        switch[s] = 0
-    else:
-        switch[s] = 1
+import sys
+input = sys.stdin.readline
 
+N, K = map(int, input().split())  # N:전체날짜 K:연속날짜수
+tmp = list(map(int, input().split()))
+sum_tmp = sum(tmp[:K])
+res = sum_tmp
 
-def switching(s, n):
-    if s == 1:  # 남자면
-        for i in range(n-1, T, n):
-            on_off(i)
-    else:  # 여자면
-        max_len = min(n-1, T - n)
-        print(max_len)
-        while max_len:
-            if switch[n - 1 - max_len:n - 1] == switch[n - 1 + max_len:n - 1:-1]:
-                for i in range(n - 1 - max_len, n - 1 + max_len + 1):
-                    on_off(i)
-                break
-            max_len -= 1
+for i in range(1, N - K + 1):
+    sum_tmp = sum_tmp - tmp[i - 1] + tmp[i + K - 1]
+    res = max(res, sum_tmp)
 
-
-T = int(input())  # 스위치 개수
-switch = list(map(int, input().split()))
-n = int(input())  # 학생 수
-for _ in range(n):
-    s, sw_n = map(int, input().split())  # 남학생 s=1, 여학생 s=2
-    switching(s, sw_n)
-
-print(*switch)
-
+print(res)
