df92643f4ba91e4c24601efa1763a06c71038844
bada35 <diduedidue@naver.com>
2023-09-06 18:00:41 +0900

    jip

--- SSAFY/Algorithm/ETC/test.py
+++ SSAFY/Algorithm/ETC/test.py
@@ -1,55 +1,44 @@
+from itertools import combinations
 import sys
-# input = sys.stdin.readline
-sys.setrecursionlimit(10**4)
+input = sys.stdin.readline
 
 
-def preorder_return(val, id=1, left=float('-inf'), right=float('inf')):
-    # val = [50, 30, 24, 5, 28, 45, 98, 52, 60]
-    # 일단 1번 idx부터, 첫값이니까 범위는 -무한대 ~ +무한대
-    if not val:  # 전위 순회로 얻은 값이 없으면
-        return
-
-    # 값이 지금 넣을 노드 값인지 아닌지
-    # 아니면 넣을 수 있을 곳까지 돌아가기
-    num = val[0]
-    if num < left or num > right:
-        return
-
-    val.pop(0)
-    # 노드 딕셔너리에 값 할당
-    node[id] = num
-
-    # 왼쪽 자식 노드 처리
-    c1[id] = id * 2
-    preorder_return(val, c1[id], left, num)
-
-    # 오른쪽 자식 노드 처리
-    c2[id] = id * 2 + 1
-    preorder_return(val, c2[id], num, right)
+def brute_force(k, w, u):  # U안에서 K길이만큼의 부분집합을 다 만들어서 걍 다 비교(돌아갈까?? => 5% 안 돌아간다)
+    max_cnt = float('-inf')
+    for subset in combinations(u, k):
+        cnt = 0
+        for word in words:
+            if word.issubset(subset):
+                cnt += 1
+        max_cnt = max(max_cnt, cnt)
+    print(max_cnt)
 
 
-def postorder(node, c1, c2, id):
-    if id not in node:
-        return
+def init():
+    N, K = map(int, input().split())  # N:단어수(자연수 <= 50)  K:글자수(0 or 자연수 <= 26)
 
-    postorder(node, c1, c2, c1.get(id, None))
-    postorder(node, c1, c2, c2.get(id, None))
-    print(node[id])
+    if K < 5:
+        print(0)
+        exit(0)
+    else:
+        K -= 5
+
+    s = 'antic'
+    words = []
+
+    for i in range(N):
+        tmp = list(input().strip())
+        # tmp = tmp[4:len(tmp)-4]  굳이 자를필요 없네
+        w = {w for w in tmp if w not in s}
+        if len(w) <= K:  # 애초에 K보다 길이 긴 건 고려할 필요 X => 이렇게 해도 brute-force는 안됨
+            words.append(w)
+    U = set()
+    for s in words:
+        U = U.union(s)
+    return K, words, U
 
 
-val = []
-while 1:
-    try:
-        val.append(int(input().strip()))
-    except EOFError:
-        break
-# print(val)
 
-node = {}
-c1 = {}
-c2 = {}
-preorder_return(val)
-postorder(node, c1, c2, 1)
-print(node)
-print(c1)
-print(c2)
+K, words, U = init()
+brute_force(K, words, U)
+
