e1510ee28ff2254356d8b5ef03ad10f2026ab707
www-jong <weon1009@gmail.com>
2023-09-14 17:58:42 +0900

    새자리새마음

--- 0.SSAFY/Algorithm/ETC/BOJ.py
+++ 0.SSAFY/Algorithm/ETC/BOJ.py
@@ -1,67 +1,38 @@
-from itertools import combinations
-import sys
-input = sys.stdin.readline
+from collections import deque
 
+N, K = map(int, input().split())
+belts = deque(map(int, input().split()))
 
-def greedy(k, words, u):  # n자리일때 제일 많이 만들 수 있는게 n+1자리일때 제일 많이 만들 수 있는 조합에 포함되지 않을까?
-    while k:
-        max_char = ''
-        max_cnt = float('-inf')
-        st = [e for word in words for e in word]
-        for ch in u:
-            if st.count(ch) > max_cnt:
-                max_cnt = st.count(ch)
-                max_char = ch
-        for s in words:
-            s.discard(max_char)
-        k -= 1
-    cnt = 0
-    for word in words:
-        if not word:
-            cnt += 1
-    print(cnt)
+up_id = 0  # 올리는 위치
+down_id = N  # 내리는 위치
+break_cnt = 0  # 내구도 0인 개수
+robots = deque([0]*N)  # 로봇들 현재 위치 (0: 로봇 없음, 1: 로봇 있음)
+turn = 0  # 몇 번째 단계인지
 
+while break_cnt < K:
+    turn += 1
 
-def brute_force(k, words, u):  # U안에서 K길이만큼의 부분집합을 다 만들어서 걍 다 비교(돌아갈까?? => 5% 안 돌아간다)
-    max_cnt = float('-inf')
-    for subset in combinations(u, k):
-        cnt = 0
-        for word in words:
-            if word.issubset(subset):
-                cnt += 1
-        max_cnt = max(max_cnt, cnt)
-    print(max_cnt)
+    # 1. 벨트와 로봇 이동
+    belts.rotate(1)
+    robots.rotate(1)
+    robots[-1] = 0  # 로봇이 내리는 위치에 도착하면 로봇 내림
 
+    # 2. 로봇 이동
+    for i in range(N - 2, -1, -1):
+        if robots[i] and not robots[i+1] and belts[i+1]:
+            robots[i] = 0
+            robots[i+1] = 1
+            belts[i+1] -= 1
+            if belts[i+1] == 0:
+                break_cnt += 1
 
-def init():
-    N, K = map(int, input().split())  # N:단어수(자연수 <= 50)  K:글자수(0 or 자연수 <= 26)
+    robots[-1] = 0  # 로봇이 내리는 위치에 도착하면 로봇 내림
 
-    if K < 5:
-        print(0)
-        exit(0)
-    else:
-        K -= 5
+    # 3. 로봇 올리기
+    if belts[0]:
+        robots[0] = 1
+        belts[0] -= 1
+        if belts[0] == 0:
+            break_cnt += 1
 
-    s = 'antic'
-    words = []
-
-    for i in range(N):
-        tmp = list(input().strip())
-        # tmp = tmp[4:len(tmp)-4]  굳이 자를필요 없네
-        w = {w for w in tmp if w not in s}
-        if len(w) <= K:  # 애초에 K보다 길이 긴 건 고려할 필요 X => 이렇게 해도 brute-force는 안됨
-            words.append(w)
-    U = set()
-    for s in words:
-        U = U.union(s)
-    return K, words, U
-
-
-'''
-K: 1
-words: [{'r'}, {'l', 'o', 'e', 'h'}, {'r'}]
-U: {'l', 'r', 'e', 'h', 'o'}
-'''
-K, words, U = init()
-brute_force(K, words, U)
-# greedy(K, words, U)
+print(turn)
