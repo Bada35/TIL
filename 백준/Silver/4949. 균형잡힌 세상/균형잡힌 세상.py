9ed43e552cf93c4072565228271caca579d2d75a
Jisu Seo <139415935+Bada35@users.noreply.github.com>
2023-10-11 15:31:40 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 176 ms, Memory: 111400 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -0,0 +1,18 @@
+from sys import stdin
+input = stdin.readline
+
+while True:
+    s = input()
+    if s.rstrip() == '.':  # rstrip()으로 오른쪽 공백(특히 줄바꿈 문자)만 제거
+        break
+
+    li = ''.join([ch for ch in s if ch in '()[]'])
+
+    for _ in range(len(li) // 2):
+        li = li.replace('()', '')
+        li = li.replace('[]', '')
+
+    if len(li):
+        print('no')
+    else:
+        print('yes')
