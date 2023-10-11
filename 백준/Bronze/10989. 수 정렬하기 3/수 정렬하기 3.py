7312384fc08bb4012cb0a01a7bc3c4bedfd84f68
Jisu Seo <139415935+Bada35@users.noreply.github.com>
2023-10-11 14:31:41 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 3012 ms, Memory: 126204 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -7,6 +7,10 @@ s = [0] * 10001
 for _ in range(N):
     s[int(input())] += 1
 
+cnt = 0
 for i in range(1, 10001):
     for j in range(s[i]):
         print(i)
+        cnt += 1
+    if cnt == N:
+        break
