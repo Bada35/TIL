0a5be88bb6658c733ca6a61c823ad9666d029136
Bada35 <diduedidue@naver.com>
2023-11-03 17:24:30 +0900

    자리 byebye

--- 0.SSAFY/1.Algorithm/ETC/BOJ.py
+++ 0.SSAFY/1.Algorithm/ETC/BOJ.py
@@ -1,2 +1,18 @@
-A, B = input().split()
-print(max(int(A[::-1]), int(B[::-1])))
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+dol = list(map(int, input().split()))
+max_cnt = float('-inf')
+
+for j in range(N):
+    cnt = 1
+    max_num = dol[j]
+
+    for i in dol[j:]:
+        if i > max_num:
+            max_num = i
+            cnt += 1
+    max_cnt = max(max_cnt, cnt)
+
+print(max_cnt)
