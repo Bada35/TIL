8775ae6b157de8ce1a3521c25350f6af912c9483
bada35 <diduedidue@naver.com>
2023-11-13 17:48:59 +0900

    7일차뷰

---
+++ 0.SSAFY/4.Vue/07-state-management/pinia_pjt/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div></div>
+</template>
+
+<script setup>
+import { useCounterStore } from '@/stores/counter'
+
+const store = useCounterStore()
+
+console.log(store.count)
+
+</script>
+
+<style scoped>
+</style>
