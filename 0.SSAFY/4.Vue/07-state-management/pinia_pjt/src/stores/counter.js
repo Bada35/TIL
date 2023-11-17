59b6472790ec1447ef5e3c229532a58e61312e36
bada35 <diduedidue@naver.com>
2023-11-17 17:59:21 +0900

    관통1주끝

--- 0.SSAFY/4.Vue/07-state-management/pinia_pjt/src/stores/counter.js
+++ 0.SSAFY/4.Vue/07-state-management/pinia_pjt/src/stores/counter.js
@@ -2,8 +2,12 @@ import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useCounterStore = defineStore('counter', () => {
+
+  // 상태
   const count = ref(0)
   const doubleCount = computed(() => count.value * 2)
+
+  // 객체
   function increment() {
     count.value++
   }
---
+++ 0.SSAFY/4.Vue/08-01-vue-with-drf/vue-project/src/views/DetailView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script setup>
+import axios from 'axios'
+import { onMounted } from 'vue'
+import { useCounterStore } from '@/stores/counter';
+
+onMounted((
+
+) => {})
+</script>
+
+<style>
+
+</style>
