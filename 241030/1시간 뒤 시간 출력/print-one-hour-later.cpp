99928d9110699ff9e74340264e869b223739b5cb
Jisu Seo <139415935+Bada35@users.noreply.github.com>
2024-10-30 11:17:44 +0900

    Title: 1시간 뒤 시간 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241030/1시간 뒤 시간 출력/print-one-hour-later.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int h, m;
+    char temp;
+
+    cin >> h >> temp >> m;
+    cout << h + 1 << temp << m;
+    return 0;
+}
