0e88effd1cc1cd14ce686374591fd4ca2806fdd9
Jisu Seo <139415935+Bada35@users.noreply.github.com>
2024-10-31 10:24:31 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241031/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int fir, sed, thd;
+    char temp;
+
+    cin >> fir >> temp >> sed >> temp >> thd;
+    cout << "010" << temp << thd << temp << sed;
+
+    return 0;
+}
