ea9c59b2dc07c4f1a402b9a2e2a0d3cc5648f724
Jisu Seo <139415935+Bada35@users.noreply.github.com>
2024-10-31 22:41:06 +0900

    Title: 코로나 메뉴얼, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241031/코로나 메뉴얼/covid-manual.cpp
+++ 241031/코로나 메뉴얼/covid-manual.cpp
@@ -2,24 +2,17 @@
 using namespace std;
 
 int main() {
-    // 여기에 코드를 작성해주세요.
     char sym;
     int degree, cnt = 0;
 
     for (int i = 0; i < 3; i++) {
         cin >> sym >> degree;
         if ((sym == 'Y') && (degree >= 37)) {
-            cnt ++;
+            cnt++;
         }
     }
 
-    if (cnt >= 2){
-        cout << "E";
-    }
-    else {
-        cout << "N";
-    }
-
+    cout << (cnt >= 2 ? "E" : "N");
 
     return 0;
 }
