e481fe9c77b268299d965396f72ff701e98061ba
Jisu Seo <139415935+Bada35@users.noreply.github.com>
2024-11-07 10:46:32 +0900

    Title: 별 n개 출력하기, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241107/별 n개 출력하기/print-n-stars.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 0, n;
+    cin >> n;
+
+    while (i < n) {
+        cout << "*\n";
+        i++;
+    }
+    
+    return 0;
+}
