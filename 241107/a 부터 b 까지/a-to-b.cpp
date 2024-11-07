ea90d99fdf23db72cf3dc49b34c33e496ef3a8d6
Jisu Seo <139415935+Bada35@users.noreply.github.com>
2024-11-07 17:50:59 +0900

    Title: a 부터 b 까지, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

--- 241107/a 부터 b 까지/a-to-b.cpp
+++ 241107/a 부터 b 까지/a-to-b.cpp
@@ -5,7 +5,7 @@ int main() {
     int a, b;
     cin >> a >> b;
 
-    for (int i = a; i <= b; i % 2 ? i *= 2 : i += 3) {
+    for (int i = a; i <= b; i % 2 == 1 ? i *= 2 : i += 3) {
         cout << i << " ";
     }
 
