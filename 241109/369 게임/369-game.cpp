e75fd8ccd9a0dba865c2930158def775b9db8ba8
Jisu Seo <139415935+Bada35@users.noreply.github.com>
2024-11-09 19:04:20 +0900

    Title: 369 게임, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241109/369 게임/369-game.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, upper, lower;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        upper = (i / 10 == 0 ? 1 : i / 10);
+        lower = (i % 10 == 0 ? 1 : i % 10);
+        if ((upper * lower * i) % 3 == 0) {
+            cout << 0 << " ";
+        }
+        else {
+            cout << i << " ";
+        }
+    }
+    return 0;
+}
