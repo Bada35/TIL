72188894c0e3e0e0b39d4cea955afe66a232f643
Jisu Seo <139415935+Bada35@users.noreply.github.com>
2024-11-11 09:50:35 +0900

    Title: 홀수만 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241111/홀수만 출력하기/print-only-odd-numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N, num;
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        cin >> num;
+        if (num % 2 == 1 && num % 3 == 0) {
+            cout << num << endl;
+        }
+    }
+    
+    return 0;
+}
